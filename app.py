de52174759f1d7270465c05246bb435d212971b4
DenmarFel <denmarfeliciano@gmail.com>
2019-08-08 10:51:09 -0700

    Modified tables

--- app.py
+++ app.py
@@ -2,9 +2,31 @@
 This is the backend!
 '''
 from flask import Flask, jsonify, render_template, request, url_for
-from models import User
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'caponeses',
+    'db': 'postgres',
+    'host': 'hackathon-test.cqnlo7waapcg.us-east-1.rds.amazonaws.com',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), nullable=False)
+
+class Organization(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), unique=True, nullable=False)
 
 @app.route("/")
 def index():
