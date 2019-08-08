6e6985197930127728a9b7b11f47a6e41c3b0044
DenmarFel <denmarfeliciano@gmail.com>
2019-08-08 07:50:45 -0700

    Setup Flask App

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 '''
 This is the backend!
 '''
+from flask import Flask, jsonify, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__=='__main__':
+    app.run()
