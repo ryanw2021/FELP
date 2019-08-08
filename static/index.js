0fd6c9e1844f765c45ba34f7d6a00405397ba9fb
mkalil <mkalil@seas.upenn.edu>
2019-08-08 16:07:39 -0400

    Resolving conflicts

--- static/index.js
+++ static/index.js
@@ -1,7 +1,22 @@
 var events = document.getElementById('events');
-var event_container = document.createElement('div');
 var li = document.createElement('li');
 li.className = "single_event";
+var image = document.createElement('img');
+image.src = "https://www.fnasafety.com/wp-content/uploads/2018/10/chipotle-mexican-grill-logo-png-transparent.png";
+li.innerHTML = "Chipotle Fundraiser"
+var desc = document.createElement('p');
+desc.innerHTML = "A description of the event";
+desc.className = "description";
+var time = document.createElement('p');
+time.innerHTML = "Time of the event";
+time.className = "time";
+
+
+li.appendChild(image);
+li.appendChild(desc);
+li.appendChild(time);
+
+events.appendChild(li)
 
 
 
