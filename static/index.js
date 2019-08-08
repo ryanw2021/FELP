ce226b30e6be4cbfa6e6fbfe0c8a1a937453f268
mkalil <mkalil@seas.upenn.edu>
2019-08-08 15:38:59 -0400

    Madeline

---
+++ static/index.js
@@ -0,0 +1,15 @@
+var events = document.getElementById('events');
+var event_container = document.createElement('div');
+var li = document.createElement('li');
+li.className = "single_event";
+
+
+
+
+// var business_name = {business}
+// var event_time = {time}
+// var event_date = {date}
+// var event_description = {description}
+// var image_path = {image}
+
+
