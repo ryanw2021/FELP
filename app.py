98d7552727b9e2628b2f56fec949b30bf547762f
DenmarFel <denmarfeliciano@gmail.com>
2019-08-08 11:11:07 -0700

    Created more tables

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ This is the backend!
 '''
 from flask import Flask, jsonify, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+import datetime as date
 
 app = Flask(__name__)
 
@@ -27,6 +28,20 @@ class User(db.Model):
 class Organization(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(), unique=True, nullable=False)
+    fundraisers = db.relationship('Fundraiser', backref='organization', lazy=True)
+
+class Business(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), unique=True, nullable=False)
+    location = db.Column(db.String(), unique=True, nullable=False)
+    business = db.relationship('Fundraiser', backref='business', lazy=True)
+
+class Fundraiser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    business_id = db.Column(db.Integer, db.ForeignKey('business.id'), nullable=False)
+    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
+    start_date = db.Column(db.Datetime)
+    end_date = db.Column(db.Datetime)
 
 @app.route("/")
 def index():
@@ -37,4 +52,4 @@ def login():
     return render_template("login.html")
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
