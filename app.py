17cad75f347d4a954ebd623f5f1c4566d9a8e233
DenmarFel <denmarfeliciano@gmail.com>
2019-08-08 19:46:46 -0700

    Added registration capability

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-'''
-This is the backend!
-'''
 from flask import Flask, jsonify, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 import datetime as date
 
 app = Flask(__name__)
@@ -18,6 +17,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -51,6 +52,10 @@ class Follows(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 @app.route("/")
 def index():
     fundraisers = Fundraiser.query.order_by(Fundraiser.start_date).all()
@@ -64,6 +69,19 @@ def login():
 @app.route("/newAccount")
 def newAccount():
     return render_template("newAccount.html")
+
+@app.route("/newAccount", methods = ["POST"])
+def newAccount2():
+    info = request.form
+    if User.query.filter_by(email=info['email']).first() == None:
+        print("User is able to create an account!")
+        newUser = User(email=info['email'], password=info['password'], name=info['fname'] + ' ' + info['lname'] )
+        db.session.add(newUser)
+        db.session.commit()
+        print("Added New User")
+        return render_template("login.html")
+    else:
+        return render_template("newAccount.html", message="You already have an account!")
 
 @app.route("/customerProfile")
 def customerProfile():
