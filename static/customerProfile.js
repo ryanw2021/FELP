79f0ae341c663c5e2d04579a0b96027858c2dde9
Connor Calderon <cpc329@gmail.com>
2019-08-08 20:06:26 -0400

    Improved the customer profile formatting and added the search features

--- static/customerProfile.js
+++ static/customerProfile.js
@@ -1,3 +1,16 @@
 //Populate all organizations in the new organizations drop down
 //Populate a list of all currently favorited organziations
 //List email
+
+function addNewFavorite() {
+    selectedOrganization = document.getElementById('selected-organization').value
+    console.log(selectedOrganization);
+}
+
+function populateNewOrganizationList() {
+    
+}
+
+function test(){
+    console.log('here');
+}
